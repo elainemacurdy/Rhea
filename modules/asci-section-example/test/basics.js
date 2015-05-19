c4d071d4488c329280ca8c04d2d183bdb98b08fc
Elaine Harman <elaine.harman@audiencescience.com>
2015-05-19 11:05:32 -0700

    Revert "Adding very basic example section"
    This reverts commit 4a5257e6d68f5a45e83f64ed779e782605ee7f88.

--- modules/asci-section-example/test/basics.js
+++
@@ -1,7 +0,0 @@
-var test = require('tape');
-var sectionExample = require('../asci-section-example.js');
-
-test('first test', function(t) {
-  t.equal(1, 1, 'should make sure math is working');
-  t.end();
-});
